ecea1b7d864812a3cd094e7e63cb98b7d35103c1
choruk <chris@horukmail.com>
2013-10-22 18:45:03 -0700

    add devise gem, create omniauthable user, add bootstrap, add simple home controller with one action mapped to root path so devise doesnt complain about not having root route, add logout button to this home page

---
+++ papersphere/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :google]
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :provider, :uid, :name
+  # attr_accessible :title, :body
+end
