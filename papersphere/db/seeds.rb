4f4402a84769ea6e310c61c5207ed9899eca548f
Hiranya Jayathilaka <hiranya911@gmail.com>
2013-10-24 19:07:30 -0700

    Added groups model

--- papersphere/db/seeds.rb
+++ papersphere/db/seeds.rb
@@ -8,6 +8,10 @@
 
 user = User.create(:email => 'chris@test.com', :password => 'test1234', :password_confirmation => 'test1234', :name => 'Chris')
 
+user1 = User.create(:email => 'hiranya@test.com', :password => 'test1234', :password_confirmation => 'test1234', :name => 'Hiranya')
+user2 = User.create(:email => 'alex@test.com', :password => 'test1234', :password_confirmation => 'test1234', :name => 'Alex')
+user3 = User.create(:email => 'nevena@test.com', :password => 'test1234', :password_confirmation => 'test1234', :name => 'Nevena')
+
 paper1 = Paper.create(:title => 'Test Paper 1', :author => 'Author 1', :year => 2011, :publication => 'Publication 1', :url => 'http://foo1.com')
 paper2 = Paper.create(:title => 'Test Paper 2', :author => 'Author 2', :year => 2012, :publication => 'Publication 2', :url => 'http://foo2.com')
 paper3 = Paper.create(:title => 'Test Paper 3', :author => 'Author 3', :year => 2013, :publication => 'Publication 3', :url => 'http://foo3.com')
@@ -21,3 +25,8 @@ list.save!
 paper1.save!
 paper2.save!
 paper3.save!
+
+group = Group.create(:name => 'My Project Team')
+group.users = [user1, user2, user3]
+group.user = user
+group.save!
