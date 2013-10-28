0db5167c5e5dafb0a1044386e6336f81e8ab599c
Hiranya Jayathilaka <hiranya911@gmail.com>
2013-10-28 16:13:44 -0700

    UI improvements

--- papersphere/app/helpers/application_helper.rb
+++ papersphere/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def is_active(controller_name)
+    parsed_path = URI.parse(request.original_url).path
+    return 'active' if controller_name == 'reading_lists' and parsed_path == '/'
+
+    path = parsed_path.split('/')[1]
+    if controller_name == path
+      return 'active'
+    else
+      return ''
+    end
+  end
+
 end
