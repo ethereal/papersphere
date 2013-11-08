133a98274fd6487250e13879057d244f037259fa
choruk <chris@horukmail.com>
2013-11-08 12:04:05 -0800

    make rating global and add indecies on foreign keys to speed it up

--- papersphere/app/models/paper.rb
+++ papersphere/app/models/paper.rb
@@ -4,4 +4,22 @@ class Paper < ActiveRecord::Base
   attr_accessible :author, :publication, :title, :url, :year, :paper_code
 
   validates :title, :author, :year, :publication, :paper_code, :presence => true
+  
+  def average_rating
+    all_ratings = []
+    ReadingListPaper.includes(:ratings).where(:paper_id => id).all.each do |rlp|
+      rlp.ratings.each do |r|
+        all_ratings << r
+      end
+    end
+    return nil if all_ratings.empty?
+    
+    value = 0
+    all_ratings.each do |rating|
+      value += rating.value
+    end
+    total = all_ratings.size
+    (value.to_f / total.to_f).round 3
+  end
+  
 end
